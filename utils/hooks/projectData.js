157f9315cd19cd0c05ace6a305d2276e8a9a1644
Manish Jha <manishjhazyx@gmail.com>
2024-05-24 20:20:37 +0530

    chore: added new project -ai rag starwarsgpt

--- utils/hooks/projectData.js
+++ utils/hooks/projectData.js
@@ -1,139 +1,163 @@
 export const projects = [
   {
-    name: "Mini PayTM (FullStack)",
-    date: "February, 2024",
-    description: "A payments website UI and API routes with JWT Authorization middleware. Features: Sign-In & Sign-Up, View Balance, View All Users, Send Money, Protected Routes Tech: ReactJS(Vite), ExpressJS, NodeJS, MongoDB, Mongoose, TailwindCSS, React-Router, REST API, JWT",
-    liveLink: "netlify",
-    sourceLink: "https://github.com/blueanchovy/paytm"
+    name: "StarWarsGPT",
+    date: "May, 2024",
+    description:
+      "An AI RAG(Retrival Augmented Generation) Assistant to answer queries about the Star Wars Universe. Built with NextJS, Typescript, Langchain, Vercel AI SDK, OpenAI text-embedding-ada-002 model, and AstraDB vector database.",
+    liveLink: "https://starwars-rag-bot.vercel.app/",
+    sourceLink: "https://github.com/blueanchovy/starwars-rag-bot",
   },
   {
-      name: "Estatery: Real Estate Listing Website",
-      date: "July, 2022",
-      description: "Features: Search and Filter properties using attributes, Server Side Rendering, Static site generation, File based routing Tech: NextJS, Chakra CSS, Chakra UI, Bayut Real Estate API, Vercel Hosting",
-      liveLink: "https://real-estate-henna-delta.vercel.app/",
-      sourceLink: "https://github.com/blueanchovy/real-estate"
-    },
-    {
-      name: "Cred.com - UI Clone",
-      date: "February, 2022",
-      description: "A fully responsive static clone of an Indian Startup‘s website (Cred): cred.club. Tech: ReactJS, CSS3, CSS media queries, Netlify Hosting",
-      liveLink: "https://cred-clone-seven.vercel.app/",
-      sourceLink: "https://github.com/blueanchovy/cred-clone"
-    },
-    {
-      name: "Mini Slack",
-      date: "January, 2022",
-      description: "Features: Create channel, Send Messages, Switch channels, Read other’s messages, Google Authentication Tech: ReactJS, Redux, React-Router, Styled Components, Material UI, Firestore Database, Firebase Hosting",
-      liveLink: "https://manish-slack-clone.web.app/",
-      sourceLink: "https://github.com/blueanchovy/slack-clone/"
-    },
-    
-    
-    {
-      name: "Gmail UI Clone",
-      date: "January, 2022",
-      description: "Features: User authentication via Google Account, Send mail(to self, updated in feed), Open received mail Tech: ReactJS, Redux, React-router, CSS3, Firestore Database, Firebase Hosting, Material UI",
-      liveLink: "https://manish-p3-gc.web.app/",
-      sourceLink: "https://github.com/blueanchovy/gmail-clone"
-    },
-    {
-      name: "Sample Take Home Assignment",
-      date: "November, 2023",
-      description: "Built for Loch Research, translated from Figma to code. Features: Horizontal Drag, Responsiveness, HTML Dialog Tech: NextJS, SCSS, Media Queries, Vercel Hosting",
-      liveLink: "https://loch-assignment-ten.vercel.app/",
-      sourceLink: "https://github.com/blueanchovy/loch-assignment"
-    },
-    {
-      name: "How well do you know me?",
-      date: "2021",
-      description: "A CLI app built with NodeJS. A simple quiz for your friends to see how well they know you.",
-      liveLink: "https://replit.com/@blueanchovy/fromEx12?embed=1&output=1#index.js",
-      sourceLink: "https://github.com/blueanchovy/quiz-do-you-know-Manish"
-    },
-    {
-      name: "Do You know Raj Comics?",
-      date: "2021",
-      description: "A CLI app built with NodeJS. A quiz to check your knowledge of Raj Comics superheroes.",
-      liveLink: "https://replit.com/@blueanchovy/markTwo?embed=1&output=1",
-      sourceLink: "https://github.com/blueanchovy/quiz-do-you-know-RajComics"
-    },
-    {
-      name: "Minionese",
-      date: "2021",
-      description: "Are you a fan of minions? Did you know that the gibberish they say is an actual language. Use the translator to convert your text from English to Minion speak or Banana language.",
-      liveLink: "https://manish-banana-speak.netlify.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-banana-speak"
-    },
-    {
-      name: "Pig Latin Translator",
-      date: "2021",
-      description: "An English to Pig-Latin translator, which takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an ay.",
-      liveLink: "https://manish-pig-latin.netlify.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-pig-latin"
-    },
-    {
-      name: "Emoji Interpreter",
-      date: "2021",
-      description: "I struggle with understanding the emojis. Do you too? If yes, then see this library of emojis with meaning",
-      liveLink: "https://dupy1.csb.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-emoji-interpreter"
-    },
-    {
-      name: "My Favorite Movies",
-      date: "2021",
-      description: "A movie recommendation app built with ReactJS. A compendium of movies that I like, sortable by genre.",
-      liveLink: "https://izv7q.csb.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-movie-reco-app"
-    },
-    {
-      name: "Cash Register Manager",
-      date: "2021",
-      description: "A cash register manager which helps the user by telling how can he/she return the change to the customer with the minimum number of notes.",
-      liveLink: "https://manish-cash-register.netlify.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-cash-register-app"
-    },
-    {
-      name: "Is your birthday lucky?",
-      date: "2021",
-      description: "A website which can help you check whether your birthday is lucky or not by checking if the sum of its digits is divisible by your lucky number.",
-      liveLink: "https://manish-lucky-bday.netlify.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-lucky-birthday"
-    },
-    {
-      name: "Learn Triangles",
-      date: "29th September, 2021",
-      description: "A website which helps you learn about triangles.",
-      liveLink: "https://manish-triangle-app.netlify.app/index.html",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-triangle-app"
-    },
-    {
-      name: "Palindrome Birthday",
-      date: "29th September, 2021",
-      description: "An App which checks whether your birthday is a palindrome birthday or not. If not, it gives the nearest upcoming palindrome date.",
-      liveLink: "https://manish-palindrome-birthday.netlify.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-palindrome-birthday"
-    },
-    {
-      name: "Stock Profit & Loss Calculator",
-      date: "30th September, 2021",
-      description: "A website that helps the user calculate how much profit or loss he/she is making on a purchased stock in percentage & absolute values.",
-      liveLink: "https://manish-profit-loss.netlify.app/",
-      sourceLink: "https://github.com/blueanchovy/neog-camp-profit-loss-app"
-    },
-    {
-      name: "Social Media Website",
-      date: "January, 2022",
-      description: "Features: Sign Up and Sign In with Email, Upload post with caption to feed, comment on posts Tech: ReactJS, CSS3, Material UI, Firestore Database, Firebase Hosting & Storage",
-      liveLink: "https://insta-clone-2ec96.web.app/",
-      sourceLink: "https://github.com/blueanchovy/insta-clone"
-    },
-    {
-      name: "Professional Social Media Website",
-      date: "January, 2022",
-      description: "Features: Sign Up and Sign In with Email, Send Post, See Posts in feed Tech: ReactJS, CSS3, Redux, Material UI, Firestore Database, Firebase Hosting",
-      liveLink: "https://manish-linkedin-clone.web.app/",
-      sourceLink: "https://github.com/blueanchovy/linkedin-clone"
-    },
-  ];
+    name: "Mini PayTM (FullStack)",
+    date: "February, 2024",
+    description:
+      "A payments website UI and API routes with JWT Authorization middleware. Features: Sign-In & Sign-Up, View Balance, View All Users, Send Money, Protected Routes Tech: ReactJS(Vite), ExpressJS, NodeJS, MongoDB, Mongoose, TailwindCSS, React-Router, REST API, JWT",
+    liveLink: "netlify",
+    sourceLink: "https://github.com/blueanchovy/paytm",
+  },
+  {
+    name: "Estatery: Real Estate Listing Website",
+    date: "July, 2022",
+    description:
+      "Features: Search and Filter properties using attributes, Server Side Rendering, Static site generation, File based routing Tech: NextJS, Chakra CSS, Chakra UI, Bayut Real Estate API, Vercel Hosting",
+    liveLink: "https://real-estate-henna-delta.vercel.app/",
+    sourceLink: "https://github.com/blueanchovy/real-estate",
+  },
+  {
+    name: "Cred.com - UI Clone",
+    date: "February, 2022",
+    description:
+      "A fully responsive static clone of an Indian Startup‘s website (Cred): cred.club. Tech: ReactJS, CSS3, CSS media queries, Netlify Hosting",
+    liveLink: "https://cred-clone-seven.vercel.app/",
+    sourceLink: "https://github.com/blueanchovy/cred-clone",
+  },
+  {
+    name: "Mini Slack",
+    date: "January, 2022",
+    description:
+      "Features: Create channel, Send Messages, Switch channels, Read other’s messages, Google Authentication Tech: ReactJS, Redux, React-Router, Styled Components, Material UI, Firestore Database, Firebase Hosting",
+    liveLink: "https://manish-slack-clone.web.app/",
+    sourceLink: "https://github.com/blueanchovy/slack-clone/",
+  },
 
-  
+  {
+    name: "Gmail UI Clone",
+    date: "January, 2022",
+    description:
+      "Features: User authentication via Google Account, Send mail(to self, updated in feed), Open received mail Tech: ReactJS, Redux, React-router, CSS3, Firestore Database, Firebase Hosting, Material UI",
+    liveLink: "https://manish-p3-gc.web.app/",
+    sourceLink: "https://github.com/blueanchovy/gmail-clone",
+  },
+  {
+    name: "Startup Landing Page",
+    date: "November, 2023",
+    description:
+      "Take Home Assignment Built for Loch Research, translated from Figma to code. Features: Horizontal Drag, Responsiveness, HTML Dialog Tech: NextJS, SCSS, Media Queries, Vercel Hosting",
+    liveLink: "https://loch-assignment-ten.vercel.app/",
+    sourceLink: "https://github.com/blueanchovy/loch-assignment",
+  },
+  {
+    name: "How well do you know me?",
+    date: "2021",
+    description:
+      "A CLI app built with NodeJS. A simple quiz for your friends to see how well they know you.",
+    liveLink:
+      "https://replit.com/@blueanchovy/fromEx12?embed=1&output=1#index.js",
+    sourceLink: "https://github.com/blueanchovy/quiz-do-you-know-Manish",
+  },
+  {
+    name: "Do You know Raj Comics?",
+    date: "2021",
+    description:
+      "A CLI app built with NodeJS. A quiz to check your knowledge of Raj Comics superheroes.",
+    liveLink: "https://replit.com/@blueanchovy/markTwo?embed=1&output=1",
+    sourceLink: "https://github.com/blueanchovy/quiz-do-you-know-RajComics",
+  },
+  {
+    name: "Minionese",
+    date: "2021",
+    description:
+      "Are you a fan of minions? Did you know that the gibberish they say is an actual language. Use the translator to convert your text from English to Minion speak or Banana language.",
+    liveLink: "https://manish-banana-speak.netlify.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-banana-speak",
+  },
+  {
+    name: "Pig Latin Translator",
+    date: "2021",
+    description:
+      "An English to Pig-Latin translator, which takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an ay.",
+    liveLink: "https://manish-pig-latin.netlify.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-pig-latin",
+  },
+  {
+    name: "Emoji Interpreter",
+    date: "2021",
+    description:
+      "I struggle with understanding the emojis. Do you too? If yes, then see this library of emojis with meaning",
+    liveLink: "https://dupy1.csb.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-emoji-interpreter",
+  },
+  {
+    name: "My Favorite Movies",
+    date: "2021",
+    description:
+      "A movie recommendation app built with ReactJS. A compendium of movies that I like, sortable by genre.",
+    liveLink: "https://izv7q.csb.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-movie-reco-app",
+  },
+  {
+    name: "Cash Register Manager",
+    date: "2021",
+    description:
+      "A cash register manager which helps the user by telling how can he/she return the change to the customer with the minimum number of notes.",
+    liveLink: "https://manish-cash-register.netlify.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-cash-register-app",
+  },
+  {
+    name: "Is your birthday lucky?",
+    date: "2021",
+    description:
+      "A website which can help you check whether your birthday is lucky or not by checking if the sum of its digits is divisible by your lucky number.",
+    liveLink: "https://manish-lucky-bday.netlify.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-lucky-birthday",
+  },
+  {
+    name: "Learn Triangles",
+    date: "29th September, 2021",
+    description: "A website which helps you learn about triangles.",
+    liveLink: "https://manish-triangle-app.netlify.app/index.html",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-triangle-app",
+  },
+  {
+    name: "Palindrome Birthday",
+    date: "29th September, 2021",
+    description:
+      "An App which checks whether your birthday is a palindrome birthday or not. If not, it gives the nearest upcoming palindrome date.",
+    liveLink: "https://manish-palindrome-birthday.netlify.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-palindrome-birthday",
+  },
+  {
+    name: "Stock Profit & Loss Calculator",
+    date: "30th September, 2021",
+    description:
+      "A website that helps the user calculate how much profit or loss he/she is making on a purchased stock in percentage & absolute values.",
+    liveLink: "https://manish-profit-loss.netlify.app/",
+    sourceLink: "https://github.com/blueanchovy/neog-camp-profit-loss-app",
+  },
+  {
+    name: "Social Media Website",
+    date: "January, 2022",
+    description:
+      "Features: Sign Up and Sign In with Email, Upload post with caption to feed, comment on posts Tech: ReactJS, CSS3, Material UI, Firestore Database, Firebase Hosting & Storage",
+    liveLink: "https://insta-clone-2ec96.web.app/",
+    sourceLink: "https://github.com/blueanchovy/insta-clone",
+  },
+  {
+    name: "Professional Social Media Website",
+    date: "January, 2022",
+    description:
+      "Features: Sign Up and Sign In with Email, Send Post, See Posts in feed Tech: ReactJS, CSS3, Redux, Material UI, Firestore Database, Firebase Hosting",
+    liveLink: "https://manish-linkedin-clone.web.app/",
+    sourceLink: "https://github.com/blueanchovy/linkedin-clone",
+  },
+];
